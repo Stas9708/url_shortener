c14fb385962d1f6e18c82cf44d583f2da53492eb
Stas9708 <stasziba@gmail.com>
2023-06-10 21:28:32 +0300

    get_link endpoint was integrated with DB

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-"""
-- написать запрос для добавления нового юзера в БД
-"""
-
 
 import uvicorn
 from typing import Annotated
-from fastapi import FastAPI, HTTPException, Request, Form
+from fastapi import FastAPI, HTTPException, Request, Form, Depends
 from starlette.responses import RedirectResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -63,9 +59,18 @@ def item_page(request: Request, item_id: str):
 
 @app.get("/get_link")
 def get_user_link(long_link: str):
-    hash_link = str(hash(long_link))[0:5]
-    link_dict[hash_link] = {"long_link": long_link, "counter": 0}
-    return {"url": f"127.0.0.1:8080/{hash_link}"}
+    with connection.cursor() as cursor:
+        sql = "SELECT long_link, short_link FROM `links` WHERE `long_link` = %s"
+        cursor.execute(sql, (long_link,))
+        db_long_link = cursor.fetchone()
+        if db_long_link:
+            return {"url": f"127.0.0.1:8080/{db_long_link['short_link']}"}
+        else:
+            hash_link = str(hash(long_link))[0:5]
+            sql = "INSERT INTO `links` (`long_link`, `short_link`) VALUES(%s, %s)"
+            cursor.execute(sql, (long_link, hash_link))
+            connection.commit()
+            return {"url": f"127.0.0.1:8080/{hash_link}"}
 
 
 @app.get("/r/{short_link}")
